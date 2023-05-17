702f7fafc9dc4e5e83e86697602b0229aa9e99e0
azeroth <jefry43429@gmail.com>
2023-05-16 19:48:33 -0600

    fd

--- src/App.js
+++ src/App.js
@@ -4,27 +4,7 @@ import "./components/Click.js";
 import ReactHowler from "react-howler";
 import "./components/Ramdom.js";
 
-export const sizes = {
-  sm: "640px",
-  md: "768px",
-  lg: "1024px",
-  xl: "1280px",
-  "2xl": "1536px",
-};
 
-function isMatch(media) {
-  const query = `(min-width: ${sizes[media]})`;
-  return window.matchMedia(query).matches;
-}
-
-function findClosest(queries) {
-  for (let i = queries.length - 1; i >= 0; i--) {
-    if (isMatch(queries[i])) {
-      return queries[i];
-    }
-  }
-  return "sm";
-}
 
 function App() {
   return (
@@ -32,10 +12,7 @@ function App() {
       <a-assets>
         <img id="sky1" src={require("../src/texture/gal2.jpg")} />;
         <img id="meteor" src={require("../src/texture/asteroid1.jpg")} />
-        <a-asset-item
-          id="cityModel"
-          src={require("../src/models/ped.glb")}
-        ></a-asset-item>
+        
       </a-assets>
       <a-sky src="#sky1" />
 
@@ -59,12 +36,7 @@ function App() {
       ></a-entity>
       <a-camera>
         <a-cursor color="red" position="0 0 -1">
-          <a-entity
-            gltf-model="#cityModel"
-            scale="0.6 0.6 0.6"
-            position="-0.100 -1 -4"
-            rotation="-3 180.66 3"
-          ></a-entity>
+        
 
           <a-entity>
             <a-text
